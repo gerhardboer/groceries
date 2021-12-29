6548de93f88041bd6e69a1a2585a866c6e7a968d
gerhardboer <gghboer@gmail.com>
2021-12-29 11:41:12 +0100

    add angular fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'groceries-3e2b5',
+    appId: '1:857295161277:web:cbe9bee397414b2afe74d4',
+    databaseURL: 'https://groceries-3e2b5-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'groceries-3e2b5.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyA2im_VqcnkqpDsH8CR7CKJnvjrzuJASVI',
+    authDomain: 'groceries-3e2b5.firebaseapp.com',
+    messagingSenderId: '857295161277',
+  },
   production: false
 };
 
